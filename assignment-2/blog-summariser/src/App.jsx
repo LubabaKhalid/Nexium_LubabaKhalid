0e4507272d55bf77a5786aed3109789dba2ce832
lubaba <bsdsf22a025@pucit.edu.pk>
2025-07-12 13:55:28 +0500

    changes files

---
+++ assignment-2/blog-summariser/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: "2rem", fontSize: "2rem", textAlign: "center" }}>
+      ✅ Blog Summariser App is Working!
+    </div>
+  );
+}

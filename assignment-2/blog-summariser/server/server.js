2ce63cc0269a07ee464980dca4b6ff65abc9e965
lubaba <bsdsf22a025@pucit.edu.pk>
2025-07-13 23:04:50 +0500

    complete it

--- assignment-2/blog-summariser/server/server.js
+++ assignment-2/blog-summariser/server/server.js
@@ -33,7 +33,7 @@ app.post("/summarize", async (req, res) => {
         headers: {
           Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
           "Content-Type": "application/json",
-          "HTTP-Referer": "http://localhost:3000",
+          "HTTP-Referer": "https://zippy-kitten-610a98.netlify.app",
           "X-Title": "Blog Summarizer",
         },
       }
